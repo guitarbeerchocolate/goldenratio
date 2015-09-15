22f03575ea9d3a5c8810fbbc0340f8080881bc1b
guitarbeerchocolate <guitarbeerchocolate@googlemail.com>
2015-09-15 16:11:55 +0100

    home page and plugin updated

--- goldenratio.plugin.js
+++ goldenratio.plugin.js
@@ -1,16 +1,33 @@
-(function($)
-{
-  $.fn.extend(
+(function( $ ){
+  $.fn.goldenratio = function()
   {
-    goldenratio:function()
-    {
-      alert('hello');
-    }
+    
+  },
+  $.fn.setGRwidth = function()
+  {
+    $(this).width($(this).height()*1.6180);
+  },
+  $.fn.setGRheight = function()
+  {
+    $(this).height($(this).width()/1.6180);
+  },
+  $.fn.splitWidthToGR = function()
+  { 
+    var parentWidth = parseInt($(this).width());
+    var secondDivWidth = parentWidth-(parentWidth/1.6180);
+    
+    var firstDivWidth = parseInt(parentWidth-secondDivWidth);
+    
+    var $firstDiv = $('<div class="splitWidthFirst"/>');
+    var $secondDiv = $('<div class="splitWidthSecond"/>');
+    $secondDiv.width(secondDivWidth);
+    $firstDiv.width(firstDivWidth);
 
-    setGRwidth:function()
-    {
-      alert('hello');
-      $(this).width($(this).height()*1.6180);
-    };
-  });
-})(jQuery);
+    $(this).append($firstDiv);
+    $(this).append($secondDiv);
+  },
+  $.fn.splitHeightToGR = function()
+  {
+    $(this).height($(this).width()/1.6180);
+  }
+})( jQuery );
