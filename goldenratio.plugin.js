719da5b7b123f41b5b20bfe70a413bd92ccaae83
guitarbeerchocolate <guitarbeerchocolate@googlemail.com>
2015-09-15 11:39:12 +0100

    Added test page and jQuery plugin

---
+++ goldenratio.plugin.js
@@ -0,0 +1,16 @@
+(function($)
+{
+  $.fn.extend(
+  {
+    goldenratio:function()
+    {
+      alert('hello');
+    }
+
+    setGRwidth:function()
+    {
+      alert('hello');
+      $(this).width($(this).height()*1.6180);
+    };
+  });
+})(jQuery);
